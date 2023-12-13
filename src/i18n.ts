7f708632d9928a5806dc88aab740e4b9bc64b3c6
Kenny Patrick <kennypatrick96@gmail.com>
2023-12-12 21:55:40 -0600

    added homepage to package.json

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,8 +3,6 @@ import { initReactI18next } from "react-i18next";
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 
-const lng = localStorage.getItem("selectedLanguage");
-
 i18n
   .use(Backend)
   .use(initReactI18next)
@@ -17,9 +15,6 @@ i18n
     },
     interpolation: {
       escapeValue: false,
-    },
-    backend: {
-      loadPath: `/wedding-frontend/public/locales/${lng}/translation.json`,
     },
   });
 
--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: "/",
-  };
-
-  if (command != "serve") {
-    config.base = "/wedding-frontend/";
-  }
-
-  return config;
+export default defineConfig({
+  base: "/wedding-frontend",
+  plugins: [react()],
 });
