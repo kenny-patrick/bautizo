4bd3929c7ba586b55dbeeeb74ab24b5b75e0312b
Kenny Patrick <kennypatrick96@gmail.com>
2023-12-15 12:41:02 -0600

    added detail and images to dress code

--- src/components/DressCode.tsx
+++ src/components/DressCode.tsx
@@ -5,9 +5,35 @@ export default function DressCode() {
   const { t } = useTranslation();
 
   return (
-    <Box>
-      <Typography variant="h4">{t("dressCodeHeader")}</Typography>
+    <Box display="flex" flexDirection="column" alignItems="center">
+      <Typography variant="h4" alignSelf="flex-start">
+        {t("dressCodeHeader")}
+      </Typography>
+
       <Typography>{t("dressCodeBody")}</Typography>
+      <Typography>{t("dressCodeWomen")}</Typography>
+      <Typography>{t("dressCodeMen")}</Typography>
+
+      <Box display="flex" paddingTop="12px">
+        <Box
+          component="img"
+          sx={{ width: "96px" }}
+          alt="Image of an appropriate dress for women in attendance"
+          src="/wedding/img/dress.png"
+        />
+        <Box
+          component="img"
+          sx={{ width: "96px" }}
+          alt="Photo of an appropriate dress for women in attendance"
+          src="/wedding/img/suit.png"
+        />
+        <Box
+          component="img"
+          sx={{ width: "96px" }}
+          alt="Photo of an appropriate dress for women in attendance"
+          src="/wedding/img/shirt.png"
+        />
+      </Box>
     </Box>
   );
 }
