51a23cf85c69f6ff17046a3aa291b34c37e0ccc7
Kenny Patrick <kennypatrick96@gmail.com>
2023-12-15 21:04:56 -0600

    added top and bottom floral borders

--- src/components/BottomBorder.tsx
+++ src/components/BottomBorder.tsx
@@ -3,10 +3,18 @@ import { Box } from "@mui/material";
 export default function BottomBorder() {
   return (
     <Box
-      component="img"
-      sx={{ width: "200px" }}
-      alt="Floral bottom border"
-      src="/wedding/img/bottom_border1.png"
-    />
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      marginTop="-100px"
+      paddingTop="24px"
+    >
+      <Box
+        sx={{ width: "100%" }}
+        component="img"
+        alt="Floral Top Border"
+        src="/wedding/img/bottom_border1.png"
+      />
+    </Box>
   );
 }
--- src/components/TopBorder.tsx
+++ src/components/TopBorder.tsx
@@ -2,9 +2,14 @@ import { Box } from "@mui/material";
 
 export default function TopBorder() {
   return (
-    <Box display="flex" flexDirection="column" alignItems="center">
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      marginBottom="-100px"
+    >
       <Box
-        maxWidth="1920px"
+        sx={{ width: "100%" }}
         component="img"
         alt="Floral Top Border"
         src="/wedding/img/top_border1.png"
