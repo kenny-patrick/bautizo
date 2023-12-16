35d0d9555a081a62cb9a1cc25dfaad566b0d04c7
Kenny Patrick <kennypatrick96@gmail.com>
2023-12-15 19:22:40 -0600

    added dad's middle name

--- src/components/Parents.tsx
+++ src/components/Parents.tsx
@@ -7,7 +7,7 @@ export default function Parents() {
     <Box display="flex" flexDirection="column" alignItems="center">
       <Typography>{t("blessing")}</Typography>
       <Typography variant="h6">{t("groomParents")}</Typography>
-      <Typography variant="subtitle2">Brian Patrick</Typography>
+      <Typography variant="subtitle2">Brian William Patrick</Typography>
       <Typography variant="subtitle2">Veronica Sauceda de la Fuente</Typography>
       <Typography variant="h6">{t("brideParents")}</Typography>
       <Typography variant="subtitle2">Manuel Aguilar Espinosa</Typography>
