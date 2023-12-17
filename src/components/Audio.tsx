a96293935ab363a1cce76e7baf1a6fbe81e8ebfc
Kenny Patrick <kennypatrick96@gmail.com>
2023-12-17 17:15:43 -0600

    changed default volume of audio player to 50%

--- src/components/Audio.tsx
+++ src/components/Audio.tsx
@@ -2,11 +2,13 @@ import { Box } from "@mui/material";
 import { AudioPlayer } from "react-audio-play";
 
 export default function Audio() {
+  const volume = 50;
   return (
     <Box display="flex" flexDirection="column" alignItems="center">
       <AudioPlayer
         preload="none"
         width="80%"
+        volume={volume}
         src="/wedding/audio/brillas.mp3"
       />
     </Box>
