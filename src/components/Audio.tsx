289ee3ad53e3d92ed3b3a3cdbeb15bd03e13a042
Kenny Patrick <kennypatrick96@gmail.com>
2023-12-17 16:14:06 -0600

    disabled audio preload to see if it'll work on ios

--- src/components/Audio.tsx
+++ src/components/Audio.tsx
@@ -4,7 +4,11 @@ import { AudioPlayer } from "react-audio-play";
 export default function Audio() {
   return (
     <Box display="flex" flexDirection="column" alignItems="center">
-      <AudioPlayer width="80%" src="/wedding/audio/brillas.mp3" />
+      <AudioPlayer
+        preload="none"
+        width="80%"
+        src="/wedding/audio/brillas.mp3"
+      />
     </Box>
   );
 }
