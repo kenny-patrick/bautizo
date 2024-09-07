6c43172dd92e4f4271d5d6219bb99bcf6abfdcc7
Kenny Patrick <kennypatrick96@gmail.com>
2024-09-07 18:24:04 -0500

    removed purgecss

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { visualizer } from "rollup-plugin-visualizer";
-import pluginPurgeCss from "@mojojoejo/vite-plugin-purgecss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/bautizo/",
-  plugins: [
-    react(),
-    visualizer({ open: true, filename: "bundle-visualization.html" }),
-    pluginPurgeCss()
-  ]
+  plugins: [react()]
 });
