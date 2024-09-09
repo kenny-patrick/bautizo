1f8861517d3d271e3b49ebf4e9c2f8c0c6477154
Kenny Patrick <kennypatrick96@gmail.com>
2024-09-08 20:10:09 -0500

    added bible verse

--- src/components/BibleVerse.tsx
+++ src/components/BibleVerse.tsx
@@ -9,6 +9,11 @@ export default function BibleVerse() {
       textAlign="center"
       paddingBottom="24px"
     >
+      <Typography>
+        "Que mi hijo Te siga a Ti, Dios, y Te tema y guarde Tus mandamientos y
+        escuche Tu voz. Que él Te Sirva y sea fiel a Ti."
+      </Typography>
+      <Typography>Deuteronomio 13:4</Typography>
       <Typography paddingTop="12px">
         "Acompañamos a celebrar este día tan especial donde la luz de Dios se
         encenderá en nuestro corazón para iluminar todo el camino de nuestra
